515060c7d10d1ecc6a9a5b5f634dff42bc068621
AnasTrk <anastarak9@gmail.com>
2021-04-30 23:20:23 +0000

    BACKEND CRUD :: DONE

--- frontoffice/admin.py
+++ frontoffice/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+#importi l models huma lwelin
+from .models import *;
 # Register your models here.
+admin.site.register(Personne)
+admin.site.register(Adresse)
+admin.site.register(Bail)
+admin.site.register(Proprietaire)
+admin.site.register(Collaborateur)
+admin.site.register(Locataire)
+admin.site.register(Logement)
+admin.site.register(Maison)
+admin.site.register(Appartement)
