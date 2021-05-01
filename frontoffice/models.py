801a56c5d770269a159409485e6e18dc31e38377
AnasTrk <anastarak9@gmail.com>
2021-05-01 00:17:21 +0000

    Models::Updated

--- frontoffice/models.py
+++ frontoffice/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-#Model dyal Personne
-class Personne(models.Model):
-    nom=models.CharField(max_length=200)
-    prenom=models.CharField(max_length=200)
-    def __str__(self):
-        return self.nom + self.prenom
 #Adresse
 class Adresse(models.Model):
     ville=models.CharField(max_length=200)
@@ -16,15 +9,15 @@ class Adresse(models.Model):
     cp=models.IntegerField()
     departement=models.IntegerField()
     def __str__(self):
-        return self.ville+self.rue+self.no+self.cp+self.departement
-#Bail
-class Bail(models.Model):
-    no=models.IntegerField()
-    loyerTTC=models.FloatField()
-    date_debut=models.DateField(auto_now_add=True, blank=True)
-    duree=models.DurationField()
-    def __str_(self):
-        return self.no+self.loyerTTC+self.date_debut+self.duree
+        return self.ville+"-"+self.rue+"-"+str(self.no)+"-"+str(self.cp)+"-"+str(self.departement)
+#Model dyal Personne
+class Personne(models.Model):
+    nom=models.CharField(max_length=200)
+    prenom=models.CharField(max_length=200)
+    adresse=models.ForeignKey(Adresse, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nom + self.prenom
+
 #Proprietaire
 class Proprietaire(Personne):
     cne=models.CharField(max_length=200)
@@ -53,18 +46,31 @@ class Logement(models.Model):
     loyer=models.FloatField()
     charges=models.FloatField()
     partAgence=models.FloatField()
+    adresse=models.ForeignKey(Adresse, on_delete=models.CASCADE)
+    proprietaire=models.ForeignKey(Proprietaire, on_delete=models.CASCADE)
+    collaborateur=models.ForeignKey(Collaborateur, on_delete=models.CASCADE)
     def __str__(self):
-        return nbPieces+surface+photo+loyer+charges+partAgence
+        return str(self.nbPieces)+"-"+str(self.surface)+"-"+self.photo+"-"+str(self.loyer)+"-"+str(self.charges)+"-"+str(self.partAgence)
 #Model dyal Maison
 class Maison(Logement):
     surface_terrain=models.FloatField()
     grenier=models.BooleanField()
     def __str__(self):
-        return self.surface_terrain + self.grenier
+        return str(self.surface_terrain)+"-"+ str(self.grenier)
 #Model dyal Appartement
 class Appartement(Logement):
     etage=models.IntegerField()
     ascenseur=models.BooleanField()
     garage=models.BooleanField()
     def __str__(self):
-        return self.etage + self.ascenseur +self.garage
+        return str(self.etage)+"-"+str(self.ascenseur)+"-"+str(self.garage)
+#Bail
+class Bail(models.Model):
+    no=models.IntegerField()
+    loyerTTC=models.FloatField()
+    date_debut=models.DateField(auto_now_add=True, blank=True)
+    duree=models.DurationField()
+    locataire=models.ForeignKey(Locataire, on_delete=models.CASCADE)
+    logement=models.ForeignKey(Logement, on_delete=models.CASCADE)
+    def __str_(self):
+        return str(self.no)+"-"+str(self.loyerTTC)+"-"+str(self.date_debut)+"-"+str(self.duree)
