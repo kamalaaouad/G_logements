f6e78e08df207a4944cf6bb3ee70d7d1fc30aab9
kamalaaouad <kamalaaouad1999@gmail.com>
2021-05-01 02:08:08 +0000

    regler quelque probleme au niveau de models foreingkey et ajout des classes admin

--- frontoffice/admin.py
+++ frontoffice/admin.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 #importi l models huma lwelin
 from .models import *;
 # Register your models here.
-admin.site.register(Personne)
-admin.site.register(Adresse)
-admin.site.register(Bail)
-admin.site.register(Proprietaire)
-admin.site.register(Collaborateur)
-admin.site.register(Locataire)
-admin.site.register(Logement)
-admin.site.register(Maison)
-admin.site.register(Appartement)
+
+admin.site.register(Adresse,AdresseAdmin)
+admin.site.register(Bail,BailAdmin)
+admin.site.register(Locataire, locataireAdmin)
+admin.site.register(Collaborateur,CollaborateurAdmin)
+admin.site.register(Proprietaire,ProprietaireAdmin)
+admin.site.register(Appartement,AppartementAdmin)
+admin.site.register(Maison,MaisonAdmin)
